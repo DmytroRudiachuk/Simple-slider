8c730145e6fbc811596a75715678a4b12663cb87
keri19372 <keri19372@gmail.com>
2019-10-28 22:34:30 +0200

    finished slider!

--- script/index.js
+++ script/index.js
@@ -1,15 +1,27 @@
 let btn_prev = document.querySelector("#prev");
 let btn_next = document.querySelector("#next");
 
-let myPhoto = document.querySelectorAll(".gallery .photo img");
-let i =0;
+let myPhoto = document.querySelectorAll("#gallery .photo img");
+let i = 0;
 
 btn_next.onclick = function() {
-	myPhoto[i].slyle.display = "none";
-	i++
+	myPhoto[i].style.display = "none";
+	i++;
 
+	if(i >= myPhoto.length){
+		i = 0;
+	}
+
+	myPhoto[i].style.display = "block";
+	
 }
 
 btn_prev.onclick = function() {
-	alert(1)
+	myPhoto[i].style.display = "none"
+	i--;
+	if(i < 0){
+		i = myPhoto.length - 1;
+	}
+	myPhoto[i].style.display = "block";
+
 }
